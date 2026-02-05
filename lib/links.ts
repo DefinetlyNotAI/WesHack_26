b5bbd257dd89da3ed4b368829cf1a83b7da911e5
Shahm Najeeb <Shahm_Najeeb@outlook.com>
2026-02-05 20:21:31 +0400

    feat: update contact email in external links
    Signed-off-by: Shahm Najeeb <Shahm_Najeeb@outlook.com>

--- lib/links.ts
+++ lib/links.ts
@@ -6,7 +6,7 @@ import {ExternalLinks} from "@/types";
 
 export const EXTERNAL_LINKS: ExternalLinks = {
     registrationForm: "https://forms.gle/VoMjxshuVnct6D9J7",
-    contactEmail: "hackstreet.ae@gmail.com",
+    contactEmail: "contact@weshack.org",
     social: {
         discord: "https://discord.gg/ZTryF6SRRp",
         github: "https://github.com/DefinetlyNotAI/WesHack_26",
